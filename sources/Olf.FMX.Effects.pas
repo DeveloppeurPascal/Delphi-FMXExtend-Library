61c11f1edc78516721249156fc6cb36bf6bf8094
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2022-08-31 17:08:15 +0200

    added TOlfLiveBindedEffect as ancestor for effects with LiveBindings

--- sources/Olf.FMX.Effects.pas
+++ sources/Olf.FMX.Effects.pas
@@ -1,8 +1,18 @@
 unit Olf.FMX.Effects;
+
 interface
+
 uses
-  System.SysUtils, System.Classes, FMX.Types, FMX.Effects, FMX.Graphics, FMX.Bind.Editors, Data.Bind.Components;
+  System.SysUtils, System.Classes, FMX.Types, FMX.Effects, FMX.Graphics,
+  FMX.Bind.Editors, Data.Bind.Components;
+
 type
+  /// <summary>
+  /// To activate visual LiveBindings on inherited components.
+  /// </summary>
+  TOlfLiveBindedEffect = class(TEffect)
+  end;
+
   [ComponentPlatformsAttribute(pfidWindows or pfidOSX or pfidLinux or
     pfidAndroid or pfidiOS)]
   TOlfFlipHorizontalEffect = class(TEffect)
@@ -33,15 +43,16 @@ type
 
   [ComponentPlatformsAttribute(pfidWindows or pfidOSX or pfidLinux or
     pfidAndroid or pfidiOS)]
-  TOlfRotateEffect = class(TEffect)
+  TOlfRotateEffect = class(TOlfLiveBindedEffect)
   private
     FRotation: Single;
     procedure SetRotation(const Value: Single);
     procedure ObserverToggle(const AObserver: IObserver; const Value: Boolean);
- protected
+  protected
     { Déclarations protégées }
     function CanObserve(const ID: Integer): Boolean; override;
-    procedure ObserverAdded(const ID: Integer; const Observer: IObserver); override;
+    procedure ObserverAdded(const ID: Integer;
+      const Observer: IObserver); override;
   public
     constructor Create(AOwner: TComponent); override;
     procedure ProcessEffect(const Canvas: TCanvas; const Visual: TBitmap;
@@ -49,33 +60,40 @@ type
   published
     property Enabled;
     property trigger;
-    property Rotation : Single read FRotation write SetRotation;
+    property Rotation: Single read FRotation write SetRotation;
   end;
+
 procedure Register;
+
 implementation
+
 procedure Register;
 begin
   RegisterComponents('OlfSoftware', [TOlfFlipHorizontalEffect,
-    TOlfFlipVerticalEffect,TOlfRotateEffect]);
+    TOlfFlipVerticalEffect, TOlfRotateEffect]);
 end;
+
 { TOlfFlipHorizontalEffect }
 constructor TOlfFlipHorizontalEffect.Create(AOwner: TComponent);
 begin
   inherited;
   FEffectStyle := [TEffectStyle.DisablePaint];
 end;
+
 procedure TOlfFlipHorizontalEffect.ProcessEffect(const Canvas: TCanvas;
   const Visual: TBitmap; const Data: Single);
 begin
   inherited;
   Visual.FlipHorizontal;
 end;
+
 { TOlfFlipVerticalEffect }
 constructor TOlfFlipVerticalEffect.Create(AOwner: TComponent);
 begin
   inherited;
   FEffectStyle := [TEffectStyle.DisablePaint];
 end;
+
 procedure TOlfFlipVerticalEffect.ProcessEffect(const Canvas: TCanvas;
   const Visual: TBitmap; const Data: Single);
 begin
@@ -87,11 +105,11 @@ end;
 function TOlfRotateEffect.CanObserve(const ID: Integer): Boolean;
 begin
   case ID of
-      TObserverMapping.EditLinkID, TObserverMapping.ControlValueID:
-        Result := True;
-    else
-      Result := False;
-    end;
+    TObserverMapping.EditLinkID, TObserverMapping.ControlValueID:
+      Result := True;
+  else
+    Result := False;
+  end;
 end;
 
 constructor TOlfRotateEffect.Create(AOwner: TComponent);
@@ -103,22 +121,22 @@ end;
 procedure TOlfRotateEffect.ObserverAdded(const ID: Integer;
   const Observer: IObserver);
 begin
-   if ID = TObserverMapping.EditLinkID then
-      Observer.OnObserverToggle := ObserverToggle;
+  if ID = TObserverMapping.EditLinkID then
+    Observer.OnObserverToggle := ObserverToggle;
 end;
 
 procedure TOlfRotateEffect.ObserverToggle(const AObserver: IObserver;
   const Value: Boolean);
 var
-    LEditLinkObserver: IEditLinkObserver;
+  LEditLinkObserver: IEditLinkObserver;
+begin
+  if Value then
   begin
-    if Value then
-    begin
-      if Supports(AObserver, IEditLinkObserver, LEditLinkObserver) then
-        Enabled := not LEditLinkObserver.IsReadOnly;
-    end
-    else
-      Enabled := True;
+    if Supports(AObserver, IEditLinkObserver, LEditLinkObserver) then
+      Enabled := not LEditLinkObserver.IsReadOnly;
+  end
+  else
+    Enabled := True;
 end;
 
 procedure TOlfRotateEffect.ProcessEffect(const Canvas: TCanvas;
@@ -130,19 +148,23 @@ end;
 
 procedure TOlfRotateEffect.SetRotation(const Value: Single);
 begin
- if value<>FRotation then
+  if Value <> FRotation then
   begin
-   FRotation := Value;
-   UpdateParentEffects;
+    FRotation := Value;
+    UpdateParentEffects;
   end;
 end;
 
 initialization
-RegisterFmxClasses([TOlfFlipHorizontalEffect, TOlfFlipVerticalEffect, TOlfRotateEffect ]);
+
+RegisterFmxClasses([TOlfFlipHorizontalEffect,
+  TOlfFlipVerticalEffect, TOlfRotateEffect]);
 Data.Bind.Components.RegisterValuePropertyName
-  (TArray<TClass>.create(TOlfRotateEffect), 'Rotation', 'FMX');
+  (TArray<TClass>.Create(TOlfRotateEffect), 'Rotation', 'FMX');
 
 finalization
+
 Data.Bind.Components.UnregisterValuePropertyName
-   (TArray<TClass>.create(TOlfRotateEffect));
+  (TArray<TClass>.Create(TOlfRotateEffect));
+
 end.
