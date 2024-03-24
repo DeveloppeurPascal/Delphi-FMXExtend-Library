e881bef3e3caeb12b6ef934e8752580005378f89
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-03-24 12:55:38 +0100

    added the removing date in a comment (#24)

--- sources/Olf.FMX.Effects.pas
+++ sources/Olf.FMX.Effects.pas
@@ -1,2 +1,2 @@
 {$I '..\src\Olf.FMX.Effects.pas'}
-{$message warn 'This file is deprecated, use "../src/Olf.FMX.Effects.pas" instead of it.'}
+{$message warn 'This file is deprecated, it will be removed after th 2024-04-01. Use "../src/Olf.FMX.Effects.pas" instead of it.'}
