b3784e0d344e949fd794131a1a89eb8a749282dd
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-03-24 12:51:39 +0100

    added a constructor to TOlfSelectDirectoryDialog to initialize its properties (#26)

--- src/Olf.FMX.SelectDirectory.pas
+++ src/Olf.FMX.SelectDirectory.pas
@@ -28,6 +28,7 @@ type
   protected
   public
     function Execute: boolean;
+    constructor Create(AOwner: TComponent); override;
   published
     /// <summary>
     /// Text used as the dialog box title.
@@ -57,6 +58,14 @@ begin
 end;
 
 { TOlfSelectDirectoryDialog }
+
+constructor TOlfSelectDirectoryDialog.Create(AOwner: TComponent);
+begin
+  inherited;
+  FDirectory := '';
+  FText := '';
+  FRoot := '';
+end;
 
 function TOlfSelectDirectoryDialog.Execute: boolean;
 var
